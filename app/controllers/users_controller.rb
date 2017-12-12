a74f86a2c3d2b7d9c3a152ba424c3d0ca132e48a
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-12 17:57:07 +0900

    usersコントローラーにprofileアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
   def home
   end
 
+  def profile
+  end
+
   def move_to_top
     redirect_to root_path unless user_signed_in?
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :mypage_url, :about_user, :location, :avatar_image, :pledge_privacy, :twitter_url, :facebook_url, :youtube_url, :twitch_url)
   end
 
 end
