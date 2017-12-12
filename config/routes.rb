aa498461160e506db29cff5538ac3ca76175d478
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-12 17:58:17 +0900

    ルーティングにprofile画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "top#home"
   get "home"             => "users#home"
-  get "settings/profile" => "user#edit"
+  get "settings/profile" => "users#profile"
 
   resources :users, only: [:update]
 end
