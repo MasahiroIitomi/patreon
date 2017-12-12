59fd1b9732d27689138c563c759f7d6639849878
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-12 16:09:42 +0900

    ユーザー情報編集画面のルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top#home"
-  get "home" => "users#home"
+  get "home"             => "users#home"
+  get "settings/profile" => "user#edit"
+
+  resources :users, only: [:update]
 end
