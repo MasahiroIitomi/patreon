00230c7188b582589cf58f88f53e387f3fa79e60
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-12 13:38:15 +0900

    topコントローラーのbefore_actionを追加
    ログインしている場合にユーザーのホーム画面へリダイレクトする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,10 @@
 class TopController < ApplicationController
+  before_action :move_to_user_home
+
   def home
+  end
+
+  def move_to_user_home
+    redirect_to home_path if user_signed_in?
   end
 end
