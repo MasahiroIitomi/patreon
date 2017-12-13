1a98cdd888b107865c10658eb8bb51d14dd25074
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-13 14:46:05 +0900

    creatorモデルとuserモデルのアソシエーションの記述

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,2 +1,6 @@
 class Creator < ApplicationRecord
+
+  enum genre: { video_and_films: 0, music: 1, writing: 2, comics: 3, drawing_and_painting: 4, animation: 5, podcasts: 6, games: 7, photography: 8, comedy: 9, comedy: 10, science: 11, education: 12, crafts_and_diy: 13, dance_and_theater: 14 }
+
+  belongs_to :user
 end
