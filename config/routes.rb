feed1cabff6b104349084dc14d6146976d757c79
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-15 20:38:41 +0900

    creatorアカウント作成のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   root "top#home"
   get "home"             => "users#home"
   get "settings/profile" => "users#profile"
+  get "creators/top" => "creators#home"
   get "create/1" => "creators#create_name"
+  get "create/2" => "creators#create_creating_things"
+  get "create/3" => "creators#create_genre"
+  get "create/4" => "creators#create_adult"
+  get "create/5" => "creators#create_done"
+  post "/creators/create" => "creators#create"
+  patch "creators/update/genre" => "creators#update_genre"
+  patch "creators/update/adult" => "creators#update_adult"
 
   resources :users, only: [:update]
 end
