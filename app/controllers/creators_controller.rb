fa1d692c6ad6c6ee96d69c42a536baaf23b42df5
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-21 08:01:35 +0900

    creatorsコントローラーの編集
    updateアクションとパラメーターの受け渡しに関する記述を追記

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,5 +1,5 @@
 class CreatorsController < ApplicationController
-  before_action :set_creator, only: [:home, :create_genre, :create_adult, :update_genre, :update_adult, :create_done]
+  before_action :set_creator, only: [:home, :create_genre, :create_adult, :update_genre, :update_adult, :create_done, :edit, :update]
 
 # クリエイターのトップページ
   def home
@@ -52,13 +52,26 @@ class CreatorsController < ApplicationController
     render layout: "creator_edit"
   end
 
+  def update
+    if current_user.update(user_params)
+      redirect_to action: :edit
+    else
+      render action: :edit
+    end
+  end
+
+
   private
   def set_creator
     @creator = Creator.find_by(user_id: current_user.id)
   end
 
   def creator_params
-    params.require(:creator).permit(:creating_things, :genre, :adult_content).merge(user_id: current_user.id)
+    params.require(:creator).permit(:creating_things, :is_or_are, :cover_image, :genre, :adult_content).merge(user_id: current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :mypage_url, :about_user, :location, :avatar_image, :pledge_privacy, :twitter_url, :facebook_url, :youtube_url, :twitch_url, creator_attributes: [:id, :creating_things, :is_or_are, :cover_image, :genre, :adult_content, :reward_id, :earning_privacy])
   end
 
 end
