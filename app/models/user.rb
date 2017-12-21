fde39189985a34c462f2f07570079fab8c4d858f
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-21 16:12:12 +0900

    画像の表示までの機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  mount_uploader :avatar_image, ImageUploader
+  mount_uploader :avatar_image, AvatarUploader
 
   has_one :creator
   accepts_nested_attributes_for :creator
