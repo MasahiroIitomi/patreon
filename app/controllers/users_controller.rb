6c4730452136dc787d345ab6f1852efd5cdfe852
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-22 19:58:55 +0900

    avatar画像削除機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :mypage_url, :about_user, :location, :avatar_image, :pledge_privacy, :twitter_url, :facebook_url, :youtube_url, :twitch_url)
+    params.require(:user).permit(:name, :email, :mypage_url, :about_user, :location, :avatar_image, :pledge_privacy, :twitter_url, :facebook_url, :youtube_url, :twitch_url, :remove_avatar_image)
   end
 
 end
