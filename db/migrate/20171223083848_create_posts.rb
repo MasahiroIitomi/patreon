35b8594abb974fb28e982d2f5b8f96ff34299282
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-23 17:45:24 +0900

    postsテーブルのマイグレーションファイルの作成、migrate

--- db/migrate/20171223083848_create_posts.rb
+++ db/migrate/20171223083848_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-
+      t.integer :creator_id, foreign_key: true
+      t.integer :type
+      t.string :title
+      t.text :body
+      t.integer :public_or_patron
+      t.integer :rewards_id, foreign_key: true
       t.timestamps
     end
   end
