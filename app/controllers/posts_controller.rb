1b4fda1a03fe89e0103f22f79f230dc6d2ff76d5
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-23 18:36:52 +0900

    postsコントローラーにselectアクションを追加
    それに伴う変数の値もbefore_actionで追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_creator, only: [:select]
+
+  def select
+  end
+
+  private
+  def set_creator
+    @creator = Creator.find_by(user_id: current_user.id)
+  end
 end
