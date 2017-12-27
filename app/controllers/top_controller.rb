d9a05ede9cce43d620d1a007ce0add6b831a46a7
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-27 11:42:54 +0900

    top_controllerに検索のアクションを記述

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,6 +4,14 @@ class TopController < ApplicationController
   def home
   end
 
+  def search
+    @creators = Creator.user.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def move_to_user_home
     redirect_to home_path if user_signed_in?
   end
