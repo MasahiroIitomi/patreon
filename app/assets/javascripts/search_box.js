ca9f14537dbfa0bcbcb00022322bd1b1cff1b62d
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-27 15:57:22 +0900

    searchした際のjsファイルの記述を追加
    ・appendするhtmlの記述を修正
    ・キーを押すごとにappendしたものをリセットする記述を追加

--- app/assets/javascripts/search_box.js
+++ app/assets/javascripts/search_box.js
@@ -1,4 +1,19 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+
+  function searchCreator(creator){
+    var html = `<div class="searching-creator-box">
+                  <a href="/creators/${creator.id}/top">
+                    <div class="search-left">
+                      <img src="${creator.image}">
+                    </div>
+                    <div class="search-right">
+                      <h6>${creator.name}</h6>
+                      <p>${creator.things}</p>
+                    </div>
+                  </a>
+                </div>`
+    $('#creator-search-result').append(html);
+  }
 
   $('#creator-search-field').on('keyup', function(){
     var input = $('#creator-search-field').val();
@@ -6,11 +21,20 @@ $(function() {
     if(input === spaceInput || input.length === 0){
       return false;
     }
+    $('.searching-creator-box').remove();
     $.ajax({
       type: 'GET',
       url: '/search',
       data: {keyword: input},
       dataType: 'json'
     })
-  })
+    .done(function(creators){
+      creators.forEach(function(creator){
+        searchCreator(creator);
+      })
+    })
+    .fail(function(){
+      console.log('creator検索に失敗しました。');
+    });
+  });
 });
