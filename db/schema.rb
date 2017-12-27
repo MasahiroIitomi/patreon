d9d50c3f4533cddf5be054f0d35f777e86af3c0f
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-27 18:38:32 +0900

    migrationの修正
    usersテーブルのavatar_imageカラムにデフォルトで値を保存しないよう修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223083848) do
+ActiveRecord::Schema.define(version: 20171227075818) do
 
   create_table "creators", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
