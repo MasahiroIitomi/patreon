57a5c2f334602f9c068a6d7d7f64efa039f55e05
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-28 15:37:22 +0900

    follows_controllerにフォロー作成、削除のアクションを記述

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,19 @@
 class FollowsController < ApplicationController
+  def create
+    @follow = Follow.new(
+      user_id: current_user.id,
+      creator_id: params[:creator_id]
+      )
+    @follow.save
+    redirect_to "/creators/#{params[:creator_id]}/top"
+  end
+
+  def destroy
+    @follow = Follow.find_by(
+      user_id: current_user.id,
+      creator_id: params[:creator_id]
+      )
+    @follow.destroy
+    redirect_to "/creators/#{params[:creator_id]}/top"
+  end
 end
