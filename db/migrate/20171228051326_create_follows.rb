5e2e8f0d2914645c34c0f9c84f84a7ce77cc8261
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-28 14:37:01 +0900

    followsモデルにバリデーションを追加
    presence: trueを追加

--- db/migrate/20171228051326_create_follows.rb
+++ db/migrate/20171228051326_create_follows.rb
@@ -1,8 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[5.0]
   def change
     create_table :follows do |t|
-      t.integer :user_id, foreign_key: true
-      t.integer :creator_id, foreign_key: true
+      t.integer :user_id, foreign_key: true, presence: true
+      t.integer :creator_id, foreign_key: true, presence: true
       t.timestamps
     end
   end
