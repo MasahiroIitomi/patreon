46cfb43f4efa0ddf5fdd3b2c10009b215a6d9a5a
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-29 17:20:14 +0900

    rewardsコントローラーにeditアクションを追加

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,9 @@
 class RewardsController < ApplicationController
+  def edit
+    @creatorMyself = Creator.find_by(id: current_user.creator.id)
+    render layout: "creator_edit"
+  end
+
+  def create
+  end
 end
