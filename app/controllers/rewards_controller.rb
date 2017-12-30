b6b23bbe577ae04f509b66112d66d5e44e593593
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-30 11:56:19 +0900

    rewardの新規作成のコントローラーの記述を追加

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,9 +1,21 @@
 class RewardsController < ApplicationController
   def edit
     @creatorMyself = Creator.find_by(id: current_user.creator.id)
+    @reward = Reward.new(creator_id: current_user.creator.id)
     render layout: "creator_edit"
   end
 
   def create
+    @reward = Reward.new(reward_params)
+    if @reward.save
+      redirect_to "/creators/#{@reward.creator.id}/rewards"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def reward_params
+    params.require(:reward).permit(:price, :title, :description, :image, :patron_limit, :ask_address).merge(creator_id: current_user.creator.id)
   end
 end
