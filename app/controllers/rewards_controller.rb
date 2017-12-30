310f021fbd2349f3d9ebfd13f40ab4d86088d62b
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-30 18:41:30 +0900

    [controller]rewardのupdateアクションを記述

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -15,6 +15,15 @@ class RewardsController < ApplicationController
     end
   end
 
+  def update
+    @reward = Reward.find_by(id: params[:id])
+    if @reward.update(reward_params)
+      redirect_to edit_creator_path(current_user.creator.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def reward_params
     params.require(:reward).permit(:price, :title, :description, :image, :patron_limit, :ask_address).merge(creator_id: current_user.creator.id)
