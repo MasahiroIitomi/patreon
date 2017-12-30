42eda7cf55efa928ffa88ff1425da55f48e98a9e
Masahiro Iitomi <lpr8f81230@gmail.com>
2017-12-30 17:36:23 +0900

    My tiersを表示させるための変数の記述をコントローラーに追加

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -2,6 +2,7 @@ class RewardsController < ApplicationController
   def edit
     @creatorMyself = Creator.find_by(id: current_user.creator.id)
     @reward = Reward.new(creator_id: current_user.creator.id)
+    @myRewards = Reward.where(creator_id: current_user.creator.id).order(created_at: :desc)
     render layout: "creator_edit"
   end
 
