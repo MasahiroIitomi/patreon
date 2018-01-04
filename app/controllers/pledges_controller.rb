3c7d195eabb0de399e769d3162e72f019bf3d83c
Masahiro Iitomi <lpr8f81230@gmail.com>
2018-01-04 15:58:31 +0900

    pledgesコントローラーにbe_patronアクションの記述を追加
    パトロンになるためのリンクをクリックした際に表示される最初の画面。
    金額を設定する。

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,18 @@
 class PledgesController < ApplicationController
+
+  before_action :set_creator, only:[:be_patron]
+  before_action :set_creator_myself, only:[:be_patron]
+
+  def be_patron
+  end
+
+
+  private
+  def set_creator
+    @creator = Creator.find_by(id: params[:id])
+  end
+
+  def set_creator_myself
+    @creatorMyself = Creator.find_by(user_id: current_user.id)
+  end
 end
