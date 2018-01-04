656ae4c9f2694bd25079e38bf721872ebed68cef
Masahiro Iitomi <lpr8f81230@gmail.com>
2018-01-04 20:03:06 +0900

    pledgesコントローラーのbe_patronアクションで使用する変数の設定

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -4,12 +4,17 @@ class PledgesController < ApplicationController
   before_action :set_creator_myself, only:[:be_patron]
 
   def be_patron
+    @rewards = Reward.where(creator_id: params[:creator_id]).order(created_at: :desc)
+    @pledge = Pledge.new(creator_id: params[:creator_id], user_id: current_user.id)
+  end
+
+  def create
   end
 
 
   private
   def set_creator
-    @creator = Creator.find_by(id: params[:id])
+    @creator = Creator.find_by(id: params[:creator_id])
   end
 
   def set_creator_myself
