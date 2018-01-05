8c73c4940c8dc86d6b85472c7e85c92c7200023a
Masahiro Iitomi <lpr8f81230@gmail.com>
2018-01-05 17:42:29 +0900

    be_patronアクションでのjsの挙動を追加
    ラジオボタンで選択したものの値だけ送信するように追記

--- app/assets/javascripts/be_patron.js
+++ app/assets/javascripts/be_patron.js
@@ -1,9 +1,12 @@
 $(document).on('turbolinks:load', function(){
   $('.reward-btn').change(function(){
     $('.pledge-price').css('display', 'none');
+    $('.price-form').prop('disabled', true);
     $('.reward').css({'background-color':'white', 'border': 'none'});
+
     $(this).parent('.reward__left').siblings('.reward__right').find('.pledge-price').css('display','block');
+    $(this).parent('.reward__left').parent('label').parent('.reward').find('input').prop('disabled',false);
+
     $(this).parent('.reward__left').parent('label').parent('.reward').css({'background-color':'#f3f4f5', 'border': '1px solid #E7ECF0'});
-    // $('submit-btn').prop('disabled', false);
   });
 });
